b7a3da21888ebd5b526d50e410df2c834c39c1fc
Clement <clement@garrot.org>
2019-01-22 22:49:03 +0100

    Ajout de reactstrap avec bootstrap 4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './custom.css';
 
 import Dashboard from "./components/dashboard.js"
