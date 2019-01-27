cc108d6d63f74c8cae121fba7aa5a25d24c3c79f
Clement <clement@garrot.org>
2019-01-27 23:03:54 +0100

    Composant switch

---
+++ src/components/CheckboxSwitch.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+
+class CheckboxSwitch extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <label className="switch">
+                <input type="checkbox" id={this.props.id} name={this.props.name} onClick={this.props.onClick} className={this.props.className}/>
+                <span className="slider round" />
+            </label>
+        );
+    }
+}
+
+export default CheckboxSwitch;
